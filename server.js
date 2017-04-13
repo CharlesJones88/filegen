c362b81c1237e22aa4d7f157c9bf0d8af9fb75df
Charles Jones <charlesjones1088@gmail.com>
2017-04-12 22:43:52 -0600

    Fixing typo

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.static(path.join(__dirname, 'dist')));
 app.use('/api', api);
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__direname, 'dist/index.html'));
+    res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
 const port = process.env.PORT || '8080';
